fa8bfbd4673681242383fca1a93cdb7795b82bcf
Morgan-B3 <boutroism@gmail.com>
2024-01-30 13:03:24 +0100

    fixed header/layout not showing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import Icon from './Icon';
+import { BiLeftArrowAlt } from "react-icons/bi";
 
 const Header = ({isHome = false}) => {
     const navigate = useNavigate();
 
   return (
     <header>
-        {!isHome ? <Icon name="BiLeftArrowAlt" size="8rem" className="back-button" action={() => navigate('/')} /> : ""}
+        {!isHome ? <BiLeftArrowAlt size="8rem" className="back-button" action={() => navigate('/')} /> : ""}
         <div>
             <img src={require("../img/logo-clean3000-transparent.png")} alt="" onClick={()=>navigate('/')} />
         </div>
--- src/pages/NoticeView.js
+++ src/pages/NoticeView.js
@@ -1,4 +1,4 @@
-import { Layout } from 'antd'
+import Layout from '../components/Layout'
 import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
@@ -12,14 +12,51 @@ const NoticeView = () => {
     }, []);
 
     const notices = useSelector(state => state.data.notices);
-    const notice = notices.find((notice) => notice.id === id);
+    const notice = notices.find((notice) => notice.id == id);
 
     console.log(notice);
     console.log(id);
 
     return (
         <Layout>
-            {/* <div>{notice.client}</div> */}
+            <h1>Avis de passage n°{id}</h1>
+                    <div className='container'>
+                        <div className='container'>
+
+                            <div className='field'>
+                                <label htmlFor="">Nom du client / Entreprise :</label>
+                                <p>{notice.client}</p>
+                            </div>
+
+                        </div>
+
+                        <div className='field'>
+                            <label htmlFor="">Date d'intervention :</label>
+                            <p>{notice.date}</p>
+                        </div>
+                    </div>
+
+
+                    <div className='field'>
+                        <label htmlFor="">Observations :</label>
+                        <p>{notice.observation}</p>
+                    </div>
+
+
+                    <div>
+                        <div className='field'>
+                            <label htmlFor="">Signature du technicien :
+                            </label>
+                            <input type="text" />
+                        </div>
+                        <div className='field'>
+                            <label htmlFor="">Signature du client /<br/>Cachet de l'entrprise :</label>
+                            <input type="text" />
+                        </div>
+                    </div>
+
+                    <input type="submit" value="Enregistrer" />
+
         </Layout>
     )
 }
